370dd0b773ac298a80e3a967c461a82b223bec7b
Mibsling <48335194+Mibsling@users.noreply.github.com>
2019-04-29 14:20:16 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogFood
+{
+   public class Dog
+    {
+        public decimal WetPrice = 2;
+        public decimal DryPrice = 1.5M;
+        public decimal WetFoodQty;
+        public decimal DryFoodQty;
+        public string Name;
+
+        public decimal DayTotal()
+        {
+            decimal DayOutput = (WetFoodQty * WetPrice) + (DryPrice * DryFoodQty);
+            return DayOutput;
+        }
+
+        public decimal WeekTotal()
+        {
+
+            return DayTotal() * 7;
+        }
+    }
+}
